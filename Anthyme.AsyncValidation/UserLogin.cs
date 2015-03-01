57bf62d55ee1abd8523c615b8f06b55610705112
anthyme <anthyme@live.com>
2015-03-01 18:58:28 +0100

    add to github

---
+++ Anthyme.AsyncValidation/UserLogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anthyme.AsyncValidation
+{
+    public class UserInput : ValidatableModel
+    {
+        private string _userName;
+        private string _email;
+        private string _repeatEmail;
+
+        [Required]
+        [StringLength(20)]
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; RaisePropertyChanged("UserName"); }
+        }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(60)]
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; RaisePropertyChanged("Email"); }
+        }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(60)]
+        [CustomValidation(typeof(UserInput), "SameEmailValidate")]
+        public string RepeatEmail
+        {
+            get { return _repeatEmail; }
+            set { _repeatEmail = value; RaisePropertyChanged("RepeatEmail"); }
+        }
+
+        public static ValidationResult SameEmailValidate(object obj, ValidationContext context)
+        {
+            var user = (UserInput)context.ObjectInstance;
+            if (user.Email != user.RepeatEmail)
+            {
+                return new ValidationResult("The emails are not equal", new List<string> { "Email", "RepeatEmail" });
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Anthyme.AsyncValidation/ValidatableModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anthyme.AsyncValidation
+{
+    public class ValidatableModel : INotifyDataErrorInfo, INotifyPropertyChanged
+    {
+        private ConcurrentDictionary<string, List<string>> _errors = new ConcurrentDictionary<string, List<string>>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            ValidateAsync();
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public void OnErrorsChanged(string propertyName)
+        {
+            var handler = ErrorsChanged;
+            if (handler != null)
+                handler(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            List<string> errorsForName;
+            _errors.TryGetValue(propertyName, out errorsForName);
+            return errorsForName;
+        }
+
+        public bool HasErrors
+        {
+            get { return _errors.Any(kv => kv.Value != null && kv.Value.Count > 0); }
+        }
+
+        public Task ValidateAsync()
+        {
+            return Task.Run(() => Validate());
+        }
+
+        private object _lock = new object();
+        public void Validate()
+        {
+            lock (_lock)
+            {
+                var validationContext = new ValidationContext(this, null, null);
+                var validationResults = new List<ValidationResult>();
+                Validator.TryValidateObject(this, validationContext, validationResults, true);
+
+                foreach (var kv in _errors.ToList())
+                {
+                    if (validationResults.All(r => r.MemberNames.All(m => m != kv.Key)))
+                    {
+                        List<string> outLi;
+                        _errors.TryRemove(kv.Key, out outLi);
+                        OnErrorsChanged(kv.Key);
+                    }
+                }
+
+                var q = from r in validationResults
+                        from m in r.MemberNames
+                        group r by m into g
+                        select g;
+
+                foreach (var prop in q)
+                {
+                    var messages = prop.Select(r => r.ErrorMessage).ToList();
+
+                    if (_errors.ContainsKey(prop.Key))
+                    {
+                        List<string> outLi;
+                        _errors.TryRemove(prop.Key, out outLi);
+                    }
+                    _errors.TryAdd(prop.Key, messages);
+                    OnErrorsChanged(prop.Key);
+                }
+            }
+        }
+    }
+}
